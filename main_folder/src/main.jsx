e832783526b34ebd5620bdb2033d52d98feef20a
Kenechukwu Ajufo <kcda4468@gmail.com>
2025-04-11 12:40:32 +0100

    Initial Commit & Push

---
+++ main_folder/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <div>
+            <h1 className="headingClass">Hello World</h1>
+        </div>
+    </React.StrictMode>
+);
+
+
