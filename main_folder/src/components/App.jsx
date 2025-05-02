683503a2772f6a79f28f7887c7a303f0795113c4
Kenechukwu <kcda4468@gmail.com>
2025-05-02 18:03:36 +0100

    Routes Implemented

--- main_folder/src/components/App.jsx
+++ main_folder/src/components/App.jsx
@@ -2,13 +2,18 @@ import HomePage from "./HomePage.jsx";
 import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import {Route, Routes} from "react-router-dom";
+import CommunityPage from "./CommunityPage.jsx";
 
 function App(){
     return(
         <React.StrictMode>
             <div>
                 <Header />
-                <HomePage />
+                <Routes>
+                    <Route path="/" element={ <HomePage /> } />
+                    <Route path="/community" element={ <CommunityPage /> } />
+                </Routes>
                 <Footer />
             </div>
         </React.StrictMode>
---
+++ main_folder/src/components/CommunityPage.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Images } from './Imports';
+import AuthTabs from "./AuthTabs.jsx";
+
+function CommunityPage() {
+    return (
+        <div>
+
+            {/* CONTAINER FOR THE HUDDLE LOGO*/}
+            <div className="huddleContainer">
+                <div className="huddleTitle">
+                    <img src={Images.huddleH} alt=""/>
+                    <span>UDDLE</span>
+                </div>
+            </div>
+
+            {/* INTRODUCTORY TEXT WITH LINKS TO THE LEARN MORE SECTION*/}
+            <div className="welcomeSection">
+                <div className="welcomeTitle">
+                    <p>WELCOME TO OUR HOME</p>
+                </div>
+                <div className="welcomeBody">
+                    <div className="welcomeBriefsContainer">
+                        <div className="welcomeBriefsTexts">
+                            <div className="welcomeBriefs1">
+                                <p>
+                                    Delve into the profound depths of our origin story, where every chapter is filled
+                                    with emotion, purpose, and resilience. Discover the people, places, and powerful
+                                    events that inspired our first steps, the transformative moments that shaped our
+                                    direction, and the deep-rooted passion that continues to drive our mission forward.
+                                    Learn how adversity became our teacher, compassion our compass, and community our
+                                    anchor.
+                                </p>
+                                <div className="welcomeBriefButton">
+                                    <button className="learnMoreButton">
+                                        LEARN MORE
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="welcomeBriefs2">
+                                <p>
+                                    Explore our journey from humble beginnings to impactful initiatives, learn about
+                                    the thoughtful processes we’ve developed to serve our community effectively, and
+                                    uncover the deeply rooted inspirations, stories, and values that continue to drive
+                                    our unwavering mission to make a meaningful and lasting difference in the lives of
+                                    those we support.
+                                </p>
+                                <div className="welcomeBriefButton">
+                                    <button className="learnMoreButton">
+                                        LEARN MORE
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* LOGIN AND CHARITY SECTION*/}
+            <div className="login-charity">
+                <AuthTabs/>
+            </div>
+
+        </div>
+    );
+}
+
+export default CommunityPage;
